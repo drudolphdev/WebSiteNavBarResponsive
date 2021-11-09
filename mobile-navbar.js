9edda8121d2b98bc3e6cd491e843be8592eb9ca5
Diego Rudolph <93336886+drudolphdev@users.noreply.github.com>
2021-11-09 20:13:42 +0100

    conclusion

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -11,7 +11,7 @@ class MobileNavbar{
         this.navLinks.forEach((link, index) => {
             link.style.animation
                 ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s ${index / 7 + 0.3}s`);
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
         });
     }
     handleClick() {
